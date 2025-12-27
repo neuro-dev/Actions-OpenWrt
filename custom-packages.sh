37744ea4a9715d04c26993ce315ed465fdecf8ec
Дмитрий <77197315+neuro-dev@users.noreply.github.com>
2025-12-27 23:07:39 +0000

    First test build custom firmware

--- custom-packages.sh
+++ custom-packages.sh
@@ -2,3 +2,10 @@
 
 git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
 mv OpenClash/luci-app-openclash $BUILD_ROOT/package
+
+git clone https://github.com/remittor/zapret-openwrt.git
+mv zapret-openwrt/luci-app-zapret $BUILD_ROOT/package
+mv zapret-openwrt/zapret-ip2net $BUILD_ROOT/package
+mv zapret-openwrt/zapret-mdig $BUILD_ROOT/package
+mv zapret-openwrt/zapret-tpws $BUILD_ROOT/package
+mv zapret-openwrt/zapret $BUILD_ROOT/package
