b48b86c6e797421876c7d37b7af7f1c5f77e8927
Дмитрий <77197315+neuro-dev@users.noreply.github.com>
2026-01-15 14:50:35 +0000

    update: streamline OpenWrt build workflow by removing unused environment variable assignments and optimizing package loading

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,11 +1,7 @@
-# Load luci-app-openclash
+# Load zapret
 
-git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
-mv OpenClash/luci-app-openclash $BUILD_ROOT/package
-
-git clone https://github.com/remittor/zapret-openwrt.git
+git clone --depth 1 --branch zap1  https://github.com/remittor/zapret-openwrt.git
 mv zapret-openwrt/luci-app-zapret $BUILD_ROOT/package
-mv zapret-openwrt/zapret-ip2net $BUILD_ROOT/package
-mv zapret-openwrt/zapret-mdig $BUILD_ROOT/package
-mv zapret-openwrt/zapret-tpws $BUILD_ROOT/package
 mv zapret-openwrt/zapret $BUILD_ROOT/package
+
+
